f064b68fbc00aa3068f5b38aea54cdcc975e951f
Fernanda Martins Viotto <140853127+Fernandamviotto@users.noreply.github.com>
2023-11-12 19:18:03 -0300

    Atualizações 12.11

--- js/custom.js
+++ js/custom.js
@@ -1,45 +1,45 @@
 
-  (function ($) {
-  
+(function ($) {
+
   "use strict";
 
-    // NAVBAR
-    $('.navbar-collapse a').on('click',function(){
-      $(".navbar-collapse").collapse('hide');
-    });
+  // NAVBAR
+  $('.navbar-collapse a').on('click', function () {
+    $(".navbar-collapse").collapse('hide');
+  });
 
-    $(function() {
-      $('.hero-slides').vegas({
-          slides: [
-              { src: 'images/slides/sincere-laugh-showing-picture-smartphone-casual-meeting-with-best-friends-restaurant-terrace.jpg' },
-              { src: 'images/happy-waitress-giving-coffee-customers-while-serving-them-coffee-shop.jpg' },
-              { src: 'images/young-female-barista-wear-face-mask-serving-take-away-hot-coffee-paper-cup-consumer-cafe.jpg' }
-          ],
-          timer: false,
-          animation: 'kenburns',
-      });
+  $(function () {
+    $('.hero-slides').vegas({
+      slides: [
+        { src: 'https://media.giphy.com/media/4ayiIWaq2VULC/giphy.gif' },
+        { src: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2Y4M3A5eWVnODRsaTA1N3B1c3FtZzJ2aGdkbjVlejAwc3lkYmh2biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/uYuNg2J4x9HUY/giphy.gif' },
+        { src: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbWNjMGQ5NW5ocTF0cjlwanJzeno2dHUwMHlhMDU2ZHIzMmdubXgyNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/114ugL1hv5p8He/giphy.gif' }
+      ],
+      timer: false,
+      animation: 'kenburns',
     });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-      var el = $(this).attr('href');
-      var elWrapped = $(el);
-      var header_height = $('.navbar').height() + 60;
-  
-      scrollToDiv(elWrapped,header_height);
-      return false;
-  
-      function scrollToDiv(element,navheight){
-        var offset = element.offset();
-        var offsetTop = offset.top;
-        var totalScroll = offsetTop-navheight;
-  
-        $('body,html').animate({
+  });
+
+  // CUSTOM LINK
+  $('.smoothscroll').click(function () {
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height() + 60;
+
+    scrollToDiv(elWrapped, header_height);
+    return false;
+
+    function scrollToDiv(element, navheight) {
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop - navheight;
+
+      $('body,html').animate({
         scrollTop: totalScroll
-        }, 300);
-      }
-    });
-  
-  })(window.jQuery);
+      }, 300);
+    }
+  });
+
+})(window.jQuery);
 
 
