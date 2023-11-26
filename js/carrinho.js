1426da0d20976267d9003a7c29a6184008232301
Fernanda Martins Viotto <140853127+Fernandamviotto@users.noreply.github.com>
2023-11-26 16:28:50 -0300

    carrinhovai

--- js/carrinho.js
+++ js/carrinho.js
@@ -4,110 +4,42 @@ if (document.readyState == 'loading') {
     ready()
 }
 
-var totalAmount = "0,00"
-
 function ready() {
-    // Botão remover produto
-    const removeCartProductButtons = document.getElementsByClassName("remove-product-button")
-    for (var i = 0; i < removeCartProductButtons.length; i++) {
-        removeCartProductButtons[i].addEventListener("click", removeProduct)
+    const removeProductButtons = document.getElementsByClassName("remove-product-button")
+    for (var i = 0; i < removeProductButtons.length; i++) {
+        removeProductButtons[i].addEventListener("click", removeProduct)
     }
 
-    // Mudança valor dos inputs
     const quantityInputs = document.getElementsByClassName("product-qtd-input")
     for (var i = 0; i < quantityInputs.length; i++) {
-        quantityInputs[i].addEventListener("change", checkIfInputIsNull)
+        quantityInputs[i].addEventListener("change", uptadeTotal)
     }
 
-    // Botão add produto ao carrinho
     const addToCartButtons = document.getElementsByClassName("button-hover-background")
     for (var i = 0; i < addToCartButtons.length; i++) {
-        addToCartButtons[i].addEventListener("click", addProductToCart)
+        addToCartButtons[i].addEventListener("click", addProcutToCart)
     }
-
-    // Botão comprar
-    const purchaseButton = document.getElementsByClassName("purchase-button")[0]
-    purchaseButton.addEventListener("click", makePurchase)
 }
+
+
+function addProcutToCart(event) {
+    const button = event.target
+    const productInfos = button.parentElement.parentElement
+    //console.log(productInfos)
+    const productTitle = productInfos.getElementsByClassName("product-title")[0]
+    console.log(productTitle)
+}
+
 
 function removeProduct(event) {
     event.target.parentElement.parentElement.remove()
-    updateTotal()
+    uptadeTotal()
 }
 
-function checkIfInputIsNull(event) {
-    if (event.target.value === "0") {
-        event.target.parentElement.parentElement.remove()
-    }
 
-    updateTotal()
-}
-
-function addProductToCart(event) {
-    const button = event.target
-    const productInfos = button.parentElement.parentElement
-    const productName = productInfos.getElementsByClassName("product-title")[0].innerText
-    const productPrice = productInfos.getElementsByClassName("product-price")[0].innerText
-
-    console.log(productInfos)
-
-    const productsCartNames = document.getElementsByClassName("cart-product-title")
-    for (var i = 0; i < productsCartNames.length; i++) {
-        if (productsCartNames[i].innerText === productName) {
-            productsCartNames[i].parentElement.parentElement.getElementsByClassName("product-qtd-input")[0].value++
-            updateTotal()
-            return
-        }
-    }
-
-    let newCartProduct = document.createElement("tr")
-    newCartProduct.classList.add("cart-product")
-
-    newCartProduct.innerHTML =
-        `
-        <td class="product-identification">
-          <img src="${productImage}" alt="${productName}" class="cart-product-image">
-          <strong class="cart-product-title">${productName}</strong>
-        </td>
-        <td>
-          <span class="cart-product-price">${productPrice}</span>
-        </td>
-        <td>
-          <input type="number" value="1" min="0" class="product-qtd-input">
-          <button type="button" class="remove-product-button">Remover</button>
-        </td>
-      `
-
-    const tableBody = document.querySelector(".cart-table tbody")
-    tableBody.append(newCartProduct)
-    updateTotal()
-
-    newCartProduct.getElementsByClassName("remove-product-button")[0].addEventListener("click", removeProduct)
-    newCartProduct.getElementsByClassName("product-qtd-input")[0].addEventListener("change", checkIfInputIsNull)
-}
-
-function makePurchase() {
-    if (totalAmount === "0,00") {
-        alert("Seu carrinho está vazio!")
-    } else {
-        alert(
-            `
-          Obrigado pela sua compra!
-          Valor do pedido: R$${totalAmount}\n
-          Volte sempre :)
-        `
-        )
-
-        document.querySelector(".cart-table tbody").innerHTML = ""
-        updateTotal()
-    }
-}
-
-// Atualizar o valor total do carrinho
-function updateTotal() {
+function uptadeTotal() {
+    let totalAmount = 0
     const cartProducts = document.getElementsByClassName("cart-product")
-    totalAmount = 0
-
     for (var i = 0; i < cartProducts.length; i++) {
         //console.log(cartProducts[i])
         const productPrice = cartProducts[i].getElementsByClassName("cart-product-price")[0].innerText.replace("R$", "").replace(",", ".")
@@ -115,7 +47,6 @@ function updateTotal() {
 
         totalAmount += productPrice * productQuantity
     }
-
     totalAmount = totalAmount.toFixed(2)
     totalAmount = totalAmount.replace(".", ",")
     document.querySelector(".cart-total-container span").innerText = "R$" + totalAmount
